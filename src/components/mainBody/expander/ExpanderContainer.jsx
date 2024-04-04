c85ebd6171186476bb62924dc45b84932c7c7089
akash-panthri <akashpanthri2000@gmail.com>
2024-04-05 04:57:24 +0530

    Populated data in Expander element

--- src/components/mainBody/expander/ExpanderContainer.jsx
+++ src/components/mainBody/expander/ExpanderContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ExpanderElement from './ExpanderElement'
-
+import expanderData from '../../../data/expanderData.json'
 export default function ExpanderContainer() {
   return (
-    <div className='my-[5vh]'>ExpanderContainer
-    <ExpanderElement />
+    <div className='my-[5vh]'>
+    {expanderData?.map((expanderDatum, index)=>{
+      return <ExpanderElement key={index}  expanderDatum={expanderDatum}  />
+    })}
     </div>
   )
 }
--- src/components/mainBody/expander/ExpanderElement.jsx
+++ src/components/mainBody/expander/ExpanderElement.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-export default function ExpanderElement() {
+export default function ExpanderElement({expanderDatum}) {
   return (
-    <div className='w-full border flex flex-row px-[20px] justify-between text-2xl  items-center'>
-    <span>
-    ExpanderElement
+    <div className='w-full border flex flex-row px-[20px] py-[10px] my-[10px] justify-between rounded-[10px] items-center'>
+    <span className=' text-xl'>
+    {expanderDatum}
     </span> 
-    <FontAwesomeIcon icon={faChevronRight} />
+    <FontAwesomeIcon className='text-2xl' icon={faChevronRight} />
     </div>
   )
 }
